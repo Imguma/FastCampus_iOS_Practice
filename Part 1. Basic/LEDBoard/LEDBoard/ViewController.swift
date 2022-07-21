8b99da96652219456d85341cfaf6ae579e28bce0
임가영 <lgy0530@naver.com>
2022-07-21 21:04:57 +0900

    LED Display App

---
+++ Part 1. Basic/LEDBoard/LEDBoard/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by 임가영 on 2022/07/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+
+    @IBOutlet weak var contentsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.contentsLabel.textColor = .yellow
+    }
+    
+    // 세그웨이 화면전환을 사용했기 때문에 prepare 재정의
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            // ?? .black -> 만약 옵셔널이면 블랙으로 넘겨줌
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
+}
+
